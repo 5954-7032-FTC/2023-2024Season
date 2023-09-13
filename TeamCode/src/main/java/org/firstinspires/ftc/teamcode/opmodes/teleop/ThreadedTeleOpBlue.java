6e1a19af530d9f3cb74a07c2653efe3f747dee33
Greg Weaver <greg.weaver@careevolution.com>
2023-09-13 14:01:17 -0400

    initial command commit (#2)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/ThreadedTeleOpBlue.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/ThreadedTeleOpBlue.java
@@ -2,8 +2,7 @@ package org.firstinspires.ftc.teamcode.opmodes.teleop;
 
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
-import org.firstinspires.ftc.teamcode.hardware.LightBlue;
-import org.firstinspires.ftc.teamcode.hardware.Lights;
+import org.firstinspires.ftc.teamcode.subsystems.hardware.Lights;
 
 
 //threaded tele op controller......
@@ -13,10 +12,15 @@ import org.firstinspires.ftc.teamcode.hardware.Lights;
  * This is the threaded Tele Opmode with blue lights
  * @author Greg Weaver
  */
-public class ThreadedTeleOpBlue extends ThreadedTeleOpBase{
+public class ThreadedTeleOpBlue extends ThreadedTeleOp {
 
     @Override
     public Lights getLights() {
-        return new LightBlue(hardwareMap.dcMotor.get("LIGHTS"));
+        return new Lights(hardwareMap.dcMotor.get("LIGHTS")){
+            @Override
+            public void on() {
+                blueOn();
+            }
+        };
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/ThreadedTeleOpRed.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/ThreadedTeleOpRed.java
@@ -1,15 +1,20 @@
 package org.firstinspires.ftc.teamcode.opmodes.teleop;
 
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import org.firstinspires.ftc.teamcode.hardware.LightRed;
-import org.firstinspires.ftc.teamcode.hardware.Lights;
+import org.firstinspires.ftc.teamcode.subsystems.hardware.Lights;
+
 
 
 //threaded tele op controller......
 @TeleOp(name = "TeleOp-Red")
-public class ThreadedTeleOpRed extends ThreadedTeleOpBase {
+public class ThreadedTeleOpRed extends ThreadedTeleOp {
     @Override
     public Lights getLights() {
-        return new LightRed(hardwareMap.dcMotor.get("LIGHTS"));
+        return new Lights(hardwareMap.dcMotor.get("LIGHTS")){
+            @Override
+            public void on() {
+                redOn();
+            }
+        };
     }
 }
